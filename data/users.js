46262f55193d417562a954caa34a6aa60dc45a29
Isabella Stone <ibellarose1@gmail.com>
2023-10-12 14:38:32 -0400

    fix files

--- data/users.js
+++ data/users.js
@@ -1,8 +1,36 @@
+import { users } from "../config/mongoCollections.js";
+import { checkName, checkEmail, checkPassword, checkString } from "../helpers.js";
 
 //createUser(...)
 
 //checkUser() - in Authentication.js for now
-
+//*assuming we store emails lowercase
+export const checkUser = async (emailAddress, password) => {
+    if (!emailAddress || !password) {
+      throw 'All input fields must be provided';
+    }
+  
+    emailAddress = checkEmail(emailAddress);
+    password = checkPassword(password);
+  
+    const userCollection = await users();
+    const user = await userCollection.findOne({emailAddress: emailAddress});
+    if (user === null) {
+      throw `Either the email address or password is invalid`;
+    }
+    else {
+      let same = await bcrypt.compare(password, user.password);
+      if (same) {
+        let firstName = user.firstName;
+        let lastName = user.lastName;
+        let emailAddress = user.emailAddress
+        return {firstName, lastName, emailAddress, role};
+      }
+      else {
+        throw `Either the email address or password is invalid`;
+      }
+    }
+  };
 //getAllUsers()
 
 //getUserByID(id)
