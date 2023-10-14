0246f9c4e050f62aba783e2cc8a2cf9a089f406c
megxsan <msanford@stevens.edu>
2023-10-14 10:56:49 -0400

    Handlebar for User Sign Up

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-import express from 'express';
-const app = express();
-import configRoutes from './routes/index.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import exphbs from 'express-handlebars';
-import session from 'express-session';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// import express from 'express';
+import { dbConnection, closeConnection} from "./config/mongoConnection.js";
 
-const staticDir = express.static(__dirname + '/public');
+// const app = express();
+// // import configRoutes from './routes/index.js';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import exphbs from 'express-handlebars';
+// import session from 'express-session';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-  if (req.body && req.body._method) {
-    req.method = req.body._method;
-    delete req.body._method;
-  }
+// const staticDir = express.static(__dirname + '/public');
 
-  next();
-};
+// const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+//   if (req.body && req.body._method) {
+//     req.method = req.body._method;
+//     delete req.body._method;
+//   }
 
-app.use('/public', staticDir);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(rewriteUnsupportedBrowserMethods);
+//   next();
+// };
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// app.use('/public', staticDir);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(rewriteUnsupportedBrowserMethods);
 
-configRoutes(app);
+// app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
 
-app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
-});
+// configRoutes(app);
+const database = await dbConnection();
+await database.dropDatabase();
+await closeConnection();
+
+// app.listen(3000, () => {
+//   console.log("We've now got a server!");
+//   console.log('Your routes will be running on http://localhost:3000');
+// });
