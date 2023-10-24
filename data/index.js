74540ff53f60163304d688c2e78c50a458cbca44
mdardir <mariamdardir@gmail.com>
2023-10-24 19:11:01 -0400

    created seed file and added dependencies

---
+++ data/index.js
@@ -0,0 +1,7 @@
+import * as businessDataFunctions from './business.js';
+import * as userDataFunctions from './users.js';
+import * as attractionDataFunctions from './attractions.js';
+
+export const userData = userDataFunctions;
+export const businessData = businessDataFunctions;
+export const attractionData = attractionDataFunctions;
