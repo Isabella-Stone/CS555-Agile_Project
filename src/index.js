d016bd7a9755c4cf94bf37f78565e12318803135
megxsan <msanford@stevens.edu>
2023-10-30 15:45:18 -0400

    upload image ui pops up, working to get uploaded image to save

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import {fill} from "@cloudinary/url-gen/actions/resize";
 
 const cld = new Cloudinary({
   cloud: {
-    cloudName: 'demo'
+    cloudName: 'djllvfvts'
   }
 });
 
-const myImage = cld.image('docs/models'); 
-myImage.resize(fill().width(250).height(250));
-const imgElement = document.createElement('img');
-document.body.appendChild(imgElement);
-imgElement.src = myImage.toURL();
+// const myImage = cld.image('docs/models'); 
+// myImage.resize(fill().width(250).height(250));
+// const imgElement = document.createElement('img');
+// document.body.appendChild(imgElement);
+// imgElement.src = myImage.toURL();
