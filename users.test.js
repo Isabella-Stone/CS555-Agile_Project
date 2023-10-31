67549d1fd079eaad15e030be747f49d786092a6d
Kashmeeber <areeb.ch2001@gmail.com>
2023-10-31 12:07:58 -0400

    Added test functions for editUserInfo

--- users.test.js
+++ users.test.js
@@ -1,4 +1,4 @@
-import {createUser,checkUser,getUserById, getAllUsers, getUserByEmail} from "./data/users";
+import {createUser,checkUser,getUserById, getAllUsers, getUserByEmail, editUserInfo} from "./data/users";
 import {createAttraction,get} from "./data/attractions";
 import {} from "./helpers";
 import { ObjectId } from "mongodb";
@@ -91,4 +91,32 @@ test("gets user by id", async () => {
     const Bryan = await createUser("Bryan", "Smith", "smithb@outlook.com", "Comedy?2", "bsmith", 18);
     const getUser = await getUserById(Bryan._id);
     expect(getUser).toStrictEqual({_id: Bryan._id, firstName: "Bryan", lastName: "Smith", emailAddress: "smithb@outlook.com", username: "bsmith", password: Bryan.password, age: 18, points: 0});
+})
+
+test("Edits user information", async () => {
+    const Areeb = await createUser("Areeb", "Chaudhry", "areeb1@gmail.com", "Qwertyuiop@123", "kash1234", 21)
+    const newAreeb = await editUserInfo(Areeb._id, "Areeb", "Chaudhry", "areeberz@gmail.com", "Qwertyuiop@123", "kashmeeber", 21);
+    expect(newAreeb).toStrictEqual({_id: newAreeb._id, firstName: "Areeb", lastName: "Chaudhry", emailAddress: "areeberz@gmail.com", username: "kashmeeber", password: newAreeb.password, age: 21, points: 0});
+})
+
+test("Should not allow user to change email into one that already exists", async () => {
+    let errorz;
+    try {
+        const blah = await createUser("Areeb", "Chaudhry", "blah@gmail.com", "Qwertyuiop@123", "blah123", 21)
+        const newAreeb = await editUserInfo(blah._id, "Areeb", "Chaudhry", "areeberz@gmail.com", "Qwertyuiop@123", "blah123", 21);
+    } catch (e) {
+        errorz = e;
+    }
+    expect(errorz).toStrictEqual("Email already exists (updateUser)");
+})
+
+test("Should not allow user to change username into one that already exists", async () => {
+    let error1;
+    try {
+        const blah2 = await createUser("Areeb", "Chaudhry", "blah2@gmail.com", "Qwertyuiop@123", "blah1234", 21)
+        const newAreeb = await editUserInfo(blah2._id, "Areeb", "Chaudhry", "blah2@gmail.com", "Qwertyuiop@123", "blah123", 21);
+    } catch (e) {
+        error1 = e;
+    }
+    expect(error1).toStrictEqual("Username already exists (updateUser)");
 })
